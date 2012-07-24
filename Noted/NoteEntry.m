0ec97e43c0aa661c8199a29c0dec8d8fce54e1b7
Tony Hillerson <tony.hillerson@gmail.com>
2012-07-23 22:14:35 -0600

    Making NoteEntry return real values from helper methods

--- Noted/NoteEntry.m
+++ Noted/NoteEntry.m
@@ -8,39 +8,39 @@
 
 #import "NoteEntry.h"
 #import "Utilities.h"
+#import "NoteData.h"
 
 @implementation NoteEntry
 
-@synthesize  fileURL = _fileURL;
-@synthesize  noteData = _noteData;
-@synthesize state = _state;
-@synthesize version = _version;
-@synthesize typeOfCell = _typeOfCell;
-@synthesize adding = _adding;
-@synthesize moving = _moving;
+@synthesize fileURL;
+@synthesize noteData;
+@synthesize state;
+@synthesize version;
+@synthesize typeOfCell;
+@synthesize adding;
+@synthesize moving;
 
--(id)initWithFileURL:(NSURL *)fileURL noteData:(NoteData *)noteData state:(UIDocumentState)state version:(NSFileVersion *)version {
+-(id)initWithFileURL:(NSURL *)furl noteData:(NoteData *)ndata state:(UIDocumentState)st version:(NSFileVersion *)v {
     if ((self = [super init])) {
-        self.fileURL = fileURL;
-        self.noteData = noteData;
-        self.state = state;
-        self.version = version;
+        self.fileURL = furl;
+        self.noteData = ndata;
+        self.state = st;
+        self.version = v;
     }
     return self;
 }
 
 - (NSString *) title {
-    return @"title";//[entry.noteData.noteText componentsSeparatedByString:@"\n"]; <-- Grab title from there
+    return [[self.noteData.noteText componentsSeparatedByString:@"\n"] objectAtIndex:0];
 }
 
 - (NSString *) relativeDateString {
-    return @"foo"; //[Utilities formatRelativeDate:version.modificationDate];
+    return [Utilities formatRelativeDate:self.version.modificationDate];
 }
 
 - (NSString *) absoluteDateString {
-    return @"bar"; //[Utilities formatDate:version.modificationDate];
+    return [Utilities formatDate:self.version.modificationDate];
 }
-
 
 -(NSString*) description {
     return [[self.fileURL lastPathComponent] stringByDeletingPathExtension];
