5a1eeaca5cb573801f69dca3fee9cbb3bdcb7f8d
Nick Place <nick@manyplaces.net>
2012-11-20 11:36:13 -0700

    add "pull to add." dows not open newly created note yet

---
+++ Noted/DragToCreateViewController.h
@@ -0,0 +1,13 @@
+//
+//  DragToCreateViewController.h
+//  Noted
+//
+//  Created by Nick Place on 11/20/12.
+//  Copyright (c) 2012 Tack Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DragToCreateViewController : UIViewController
+
+@end
---
+++ Noted/DragToCreateViewController.m
@@ -0,0 +1,38 @@
+//
+//  DragToCreateViewController.m
+//  Noted
+//
+//  Created by Nick Place on 11/20/12.
+//  Copyright (c) 2012 Tack Mobile. All rights reserved.
+//
+
+#import "DragToCreateViewController.h"
+
+@interface DragToCreateViewController ()
+
+@end
+
+@implementation DragToCreateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
