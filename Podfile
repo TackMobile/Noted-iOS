7d6aee646a81aad8ee1885c846f914c45995a732
Vladimir Fleurima <vladimir.fleurima@gmail.com>
2013-07-25 12:09:34 -0600

    Upgraded to cocoapods 0.22.3 Added BlocksKit Deleted & reinstalled all cocoapods stuff

--- Podfile
+++ Podfile
@@ -1,4 +1,4 @@
-platform :ios
+platform :ios, '6.0'
 pod 'UIView+FrameAdditions', '0.0.1'
-pod "Reveal-iOS-SDK"
-
+pod 'Reveal-iOS-SDK'
+pod 'BlocksKit', '~>1.8'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// BlocksKit
+#define COCOAPODS_POD_AVAILABLE_BlocksKit
+#define COCOAPODS_VERSION_MAJOR_BlocksKit 1
+#define COCOAPODS_VERSION_MINOR_BlocksKit 8
+#define COCOAPODS_VERSION_PATCH_BlocksKit 2
+
 // Reveal-iOS-SDK
 #define COCOAPODS_POD_AVAILABLE_Reveal_iOS_SDK
 #define COCOAPODS_VERSION_MAJOR_Reveal_iOS_SDK 0
@@ -17,4 +23,10 @@
 #define COCOAPODS_VERSION_MAJOR_UIView_FrameAdditions 0
 #define COCOAPODS_VERSION_MINOR_UIView_FrameAdditions 0
 #define COCOAPODS_VERSION_PATCH_UIView_FrameAdditions 1
+
+// libffi
+#define COCOAPODS_POD_AVAILABLE_libffi
+#define COCOAPODS_VERSION_MAJOR_libffi 3
+#define COCOAPODS_VERSION_MINOR_libffi 0
+#define COCOAPODS_VERSION_PATCH_libffi 13
 
