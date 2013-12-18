9239e5c2bfd8b39b88309de0f11164d3fd2ba67a
Vladimir Fleurima <vladimir.fleurima@gmail.com>
2013-12-18 20:47:30 +0100

    Added Dropbox file importing.

--- Noted/NTDDropboxManager.m
+++ Noted/NTDDropboxManager.m
@@ -5,11 +5,15 @@
 //  Created by Vladimir Fleurima on 12/18/13.
 //  Copyright (c) 2013 Tack Mobile. All rights reserved.
 //
-
-#import "NTDDropboxManager.h"
 #import <Dropbox/Dropbox.h>
+#import <FlurrySDK/Flurry.h>
+#import "NTDDropboxManager.h"
+#import "NTDModalView.h"
+#import "NTDNote.h"
 
 static NSString *kDropboxEnabledKey = @"kDropboxEnabledKey";
+static NSString *kDropboxError = @"DropboxError";
+static NTDModalView *modalView;
 @implementation NTDDropboxManager
 
 +(void)initialize
@@ -34,6 +38,17 @@ static NSString *kDropboxEnabledKey = @"kDropboxEnabledKey";
     DBAccount *account = [[DBAccountManager sharedManager] handleOpenURL:url];
     BOOL success = (account != nil);
     [self setDropboxEnabled:success];
+    if (success) {
+        modalView = [[NTDModalView alloc] init];
+        modalView.message = @"Syncing";
+        modalView.type = NTDWalkthroughModalTypeMessage;
+        [modalView show];
+        [DBFilesystem setSharedFilesystem:[[DBFilesystem alloc] initWithAccount:account]];
+        __weak DBFilesystem *filesystem = [DBFilesystem sharedFilesystem];
+        [filesystem addObserver:self block:^{
+            if (filesystem.completedFirstSync) [self importExistingFiles];
+        }];
+    }
     return success;
 }
 
@@ -46,5 +61,40 @@ static NSString *kDropboxEnabledKey = @"kDropboxEnabledKey";
 {
     [NSUserDefaults.standardUserDefaults setBool:enabled forKey:kDropboxEnabledKey];
     [NSUserDefaults.standardUserDefaults synchronize];
+}
+
+#pragma mark - Importing
+/* This method should be more or less idempotent. */
++ (void)importExistingFiles
+{
+    static BOOL didImportExistingFiles = NO;
+    if (didImportExistingFiles) return;
+    
+    [NTDNote listNotesWithCompletionHandler:^(NSArray *notes) {
+        for (NTDNote *note in notes) {
+            DBError __autoreleasing *error;
+            DBPath *path = [[DBPath root] childPath:note.filename];
+            DBFile *file = [[DBFilesystem sharedFilesystem] createFile:path error:&error];
+            if (error) {
+                NSString *errorMsg = [NSString stringWithFormat:@"Error creating file named %@: %@", note.filename, error];
+                [Flurry logError:kDropboxError message:errorMsg error:error];
+                continue;
+            }
+            [file writeContentsOfFile:note.fileURL.path shouldSteal:NO error:&error];
+            if (error) {
+                NSString *errorMsg = [NSString stringWithFormat:@"Error copying contents of file named %@: %@", note.filename, error];
+                [Flurry logError:kDropboxError message:errorMsg error:error];
+                continue;
+            }
+            
+        }
+        didImportExistingFiles = YES;
+        [modalView dismissWithCompletionHandler:^{
+            modalView = [[NTDModalView alloc] init];
+            modalView.message = @"Dropbox Sync enabled. All of your existing notes are now inside the “Apps/TakeNoted” folder of your Dropbox";
+            modalView.type = NTDWalkthroughModalTypeBoolean;
+            [modalView show];
+        }];
+    }];
 }
 @end
