c999e65dcbda2cdf68c58e8b714c4003e2f64ea4
Vladimir Fleurima <vladimir.fleurima@gmail.com>
2014-02-13 11:08:40 -0700

    Experimenting with a full-bleed toast.

--- Noted/UIViewController+NTDToast.m
+++ Noted/UIViewController+NTDToast.m
@@ -15,7 +15,7 @@
     if ([NTDModalView isShowing]) return;
     
     UIFont *toastFont = [UIFont fontWithName:@"Avenir-Light" size:20];
-    CGSize messageSize = { .width = self.view.frame.size.width - 50, .height = 80};
+    CGSize messageSize = { .width = self.view.frame.size.width, .height = 60};
     
     // calculate the toast size
     CGRect toastRect = {
