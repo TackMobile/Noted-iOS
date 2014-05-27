0fb831865a6f7b75dc4d4be1754dd565357f8abc
nicplace <nick@manyplaces.net>
2014-05-27 11:22:19 -0600

    Updating pods and adding IAHelper pod

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -9,44 +9,38 @@
 // BlocksKit
 #define COCOAPODS_POD_AVAILABLE_BlocksKit
 #define COCOAPODS_VERSION_MAJOR_BlocksKit 2
-#define COCOAPODS_VERSION_MINOR_BlocksKit 1
-#define COCOAPODS_VERSION_PATCH_BlocksKit 0
+#define COCOAPODS_VERSION_MINOR_BlocksKit 2
+#define COCOAPODS_VERSION_PATCH_BlocksKit 3
 
 // BlocksKit/All
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_All
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_All 2
-#define COCOAPODS_VERSION_MINOR_BlocksKit_All 1
-#define COCOAPODS_VERSION_PATCH_BlocksKit_All 0
+#define COCOAPODS_VERSION_MINOR_BlocksKit_All 2
+#define COCOAPODS_VERSION_PATCH_BlocksKit_All 3
 
 // BlocksKit/Core
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_Core
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_Core 2
-#define COCOAPODS_VERSION_MINOR_BlocksKit_Core 1
-#define COCOAPODS_VERSION_PATCH_BlocksKit_Core 0
+#define COCOAPODS_VERSION_MINOR_BlocksKit_Core 2
+#define COCOAPODS_VERSION_PATCH_BlocksKit_Core 3
 
 // BlocksKit/DynamicDelegate
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_DynamicDelegate
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_DynamicDelegate 2
-#define COCOAPODS_VERSION_MINOR_BlocksKit_DynamicDelegate 1
-#define COCOAPODS_VERSION_PATCH_BlocksKit_DynamicDelegate 0
+#define COCOAPODS_VERSION_MINOR_BlocksKit_DynamicDelegate 2
+#define COCOAPODS_VERSION_PATCH_BlocksKit_DynamicDelegate 3
 
 // BlocksKit/MessageUI
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_MessageUI
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_MessageUI 2
-#define COCOAPODS_VERSION_MINOR_BlocksKit_MessageUI 1
-#define COCOAPODS_VERSION_PATCH_BlocksKit_MessageUI 0
-
-// BlocksKit/MiniFFI
-#define COCOAPODS_POD_AVAILABLE_BlocksKit_MiniFFI
-#define COCOAPODS_VERSION_MAJOR_BlocksKit_MiniFFI 2
-#define COCOAPODS_VERSION_MINOR_BlocksKit_MiniFFI 1
-#define COCOAPODS_VERSION_PATCH_BlocksKit_MiniFFI 0
+#define COCOAPODS_VERSION_MINOR_BlocksKit_MessageUI 2
+#define COCOAPODS_VERSION_PATCH_BlocksKit_MessageUI 3
 
 // BlocksKit/UIKit
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_UIKit
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_UIKit 2
-#define COCOAPODS_VERSION_MINOR_BlocksKit_UIKit 1
-#define COCOAPODS_VERSION_PATCH_BlocksKit_UIKit 0
+#define COCOAPODS_VERSION_MINOR_BlocksKit_UIKit 2
+#define COCOAPODS_VERSION_PATCH_BlocksKit_UIKit 3
 
 // Dropbox-Sync-API-SDK
 #define COCOAPODS_POD_AVAILABLE_Dropbox_Sync_API_SDK
@@ -59,6 +53,18 @@
 #define COCOAPODS_VERSION_MAJOR_FlurrySDK 4
 #define COCOAPODS_VERSION_MINOR_FlurrySDK 3
 #define COCOAPODS_VERSION_PATCH_FlurrySDK 2
+
+// FlurrySDK/FlurrySDK
+#define COCOAPODS_POD_AVAILABLE_FlurrySDK_FlurrySDK
+#define COCOAPODS_VERSION_MAJOR_FlurrySDK_FlurrySDK 4
+#define COCOAPODS_VERSION_MINOR_FlurrySDK_FlurrySDK 3
+#define COCOAPODS_VERSION_PATCH_FlurrySDK_FlurrySDK 2
+
+// IAPHelper
+#define COCOAPODS_POD_AVAILABLE_IAPHelper
+#define COCOAPODS_VERSION_MAJOR_IAPHelper 1
+#define COCOAPODS_VERSION_MINOR_IAPHelper 0
+#define COCOAPODS_VERSION_PATCH_IAPHelper 7
 
 // UIView+FrameAdditions
 #define COCOAPODS_POD_AVAILABLE_UIView_FrameAdditions
